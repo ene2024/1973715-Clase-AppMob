ac9eb475784fce6e1edd94394123c027109c8d4d
Aguilar036 <cristianantonio411@gmail.com>
2024-03-23 17:23:16 -0600

    Ejercicio 1 de Componente: Databinding

--- miApp-1973715/src/app/home/home.page.ts
+++ miApp-1973715/src/app/home/home.page.ts
@@ -9,4 +9,13 @@ export class HomePage {
 
   constructor() {}
 
+  title: string = 'Databinding';
+
+  clickMe(): void {
+    alert("Me hiciste Click!");
+  }
+
+  imageUrl: string = 'https://i.redd.it/7ksiaa4exhh01.gif';
+
+  studentName: string = '';
 }
